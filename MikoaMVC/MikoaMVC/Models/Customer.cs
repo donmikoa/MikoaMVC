dba9a0d93262808eb9193489c27e0564763c5e1d
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-02-28 09:59:36 -0500

    Created Date Annotations for the Customer Class.

--- MikoaMVC/MikoaMVC/Models/Customer.cs
+++ MikoaMVC/MikoaMVC/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MikoaMVC.Models
     public class Customer
     {
         public int id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
