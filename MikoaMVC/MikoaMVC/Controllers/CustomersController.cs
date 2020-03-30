5979f9060a95359b9e240a5f6113b2e395df9777
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-03-29 23:39:41 -0400

    Creating validations for the customer form

--- MikoaMVC/MikoaMVC/Controllers/CustomersController.cs
+++ MikoaMVC/MikoaMVC/Controllers/CustomersController.cs
@@ -55,6 +55,15 @@ namespace MikoaMVC.Controllers
         [HttpPost]
         public ActionResult Create(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- MikoaMVC/MikoaMVC/Models/Customer.cs
+++ MikoaMVC/MikoaMVC/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace MikoaMVC.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -20,6 +20,7 @@ namespace MikoaMVC.Models
 
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
