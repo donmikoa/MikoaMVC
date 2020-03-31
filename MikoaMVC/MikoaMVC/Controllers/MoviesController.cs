b6b9b4aeb7ba660ae4150eaf6b020150d3a5c76f
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-03-31 19:02:54 -0400

    Created the movie form view model

--- MikoaMVC/MikoaMVC/Controllers/MoviesController.cs
+++ MikoaMVC/MikoaMVC/Controllers/MoviesController.cs
@@ -10,6 +10,17 @@ namespace MikoaMVC.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
             var movies = GetMovies();
@@ -24,6 +35,35 @@ namespace MikoaMVC.Controllers
                 new Movie { Id = 1, Name = "Shrek" },
                 new Movie { Id = 2, Name = "Wall-e" }
             };
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                
+                    //Genres = _context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+               
+            else
+            {
+                
+            }
+
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
 
         // GET: Movies/Random
@@ -43,6 +83,18 @@ namespace MikoaMVC.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            var viewModel = new MovieFormViewModel(movie)
+            {
+                //Genres = _context.Genres.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
 
 
