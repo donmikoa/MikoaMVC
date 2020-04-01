fb0e7b8946a5c051550bf3480f933f8bd3ee06d5
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-01 18:47:09 -0400

    Created the movie data transfer objects

--- MikoaMVC/MikoaMVC/Controllers/Api/CustomersController.cs
+++ MikoaMVC/MikoaMVC/Controllers/Api/CustomersController.cs
@@ -24,26 +24,26 @@ namespace MikoaMVC.Controllers.Api
             return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
 
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            return Mapper.Map<Customer, CustomerDto>(customer);
+                return NotFound();
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
         }
         [HttpPost]
-        public CustomerDto CreateCustomer (CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
             if(!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
 
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
 
             _context.Customers.Add(customer);
             _context.SaveChanges();
             customerDto.Id = customer.Id;
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
         [HttpPut]
         public void UpdateCustomer(int id, CustomerDto customerDto)
