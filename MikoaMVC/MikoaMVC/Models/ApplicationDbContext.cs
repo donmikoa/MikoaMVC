7c74efcac44dd26df195c41a15938e8ff54e7618
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-01 19:42:15 -0400

    Worked on the css

--- MikoaMVC/MikoaMVC/Models/ApplicationDbContext.cs
+++ MikoaMVC/MikoaMVC/Models/ApplicationDbContext.cs
@@ -7,18 +7,16 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MikoaMVC.Models
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        //public DbSet<Genres> Genres { get; set; }
-
-
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
 
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
